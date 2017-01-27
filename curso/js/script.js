792022de377d4bfbd3129429de4ec2b0e34d17de
Fabricia Martins Silva <fmsilva1@stefanini.com>
2017-01-27 18:10:26 -0200

    Primeiro commit

---
+++ curso/js/script.js
@@ -0,0 +1,20 @@
+/*var resposta = confirm("Deseja excluir?");
+
+console.log(resposta);
+*/
+
+var a = 10;
+var b = 33;
+
+function somar(a, b){
+	return a+b;
+}
+
+console.log(somar(5,5));
+
+document.getElementById("btn-calcular").onclick = function(){
+	var valorA = document.getElementById("valor-a").value;
+	var valorB = document.getElementById("valor-b").value;
+
+	alert(parseInt(valorA)+parseInt(valorB));
+}
